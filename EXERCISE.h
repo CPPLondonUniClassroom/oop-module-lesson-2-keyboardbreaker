0306ec30df19afa86a80432e04b667e61cf183ed
Yu-Jin Tan <yujin_1991@hotmail.com>
2019-02-21 19:47:38 +0000

    completing pt1 and 2, require reviewing

--- EXERCISE.h
+++ EXERCISE.h
@@ -2,10 +2,33 @@
 
 //This is for part 1
 class ILogger {
-
+public:
+    virtual ILogger& LogInfo(const std::string& msg)=0;
+    virtual ILogger& LogError(const std::string& msg) = 0;
+    virtual const std::string& GetLastError() = 0;
+    virtual ~ILogger() = default;
 };
 
 // This is for Part 2/3
-class StringLogger {
+class StringLogger : public ILogger {
+private:
+    std::ostream& stream;
+    std::string lastError{};
 
+public:
+    StringLogger(std::ostream& strm) : stream{strm}{}
+    ILogger& LogInfo(const std::string& msg) override {
+        stream << "INFO: " << msg;
+        return *this;
+    }
+
+    ILogger& LogError(const std::string& msg) override {
+        stream << "ERROR: " << msg;
+        lastError = msg; //copy the passed string into lastError
+    }
+
+    const std::string& GetLastError() override {
+        return lastError;
+    }
 };
+
